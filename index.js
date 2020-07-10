263bdf5092b832377b28fd567b707c2c6f0846a4
Vladimir <vladimirnikev@gmail.com>
2020-07-10 21:26:44 +0300

    0.0009

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 // Modal
-let modalWrapper = document.getElementsByClassName("modal-wrapper")[0];
-let close = document.getElementsByClassName("close");
-let entryLink = document.getElementsByClassName("link")[3];
-let registrationLink = document.getElementsByClassName("link")[4];
-let modalContent = document.getElementsByClassName("modal-entry");
+// let modalWrapper = document.getElementsByClassName("modal-wrapper")[0];
+// let close = document.getElementsByClassName("close");
+// let entryLink = document.getElementsByClassName("link")[3];
+// let registrationLink = document.getElementsByClassName("link")[4];
+// let modalContent = document.getElementsByClassName("modal-entry");
 
-entryLink.onclick = function () {
-    modalContent[1].style.display = "none";
-    modalWrapper.style.display = "block";
-    modalContent[0].style.display = "flex";
-}
+// entryLink.onclick = function () {
+//     modalContent[1].style.display = "none";
+//     modalWrapper.style.display = "block";
+//     modalContent[0].style.display = "flex";
+// }
 
-registrationLink.onclick = function () {
-    modalContent[0].style.display = "none";
-    modalWrapper.style.display = "block";
-    modalContent[1].style.display = "flex";
-}
+// registrationLink.onclick = function () {
+//     modalContent[0].style.display = "none";
+//     modalWrapper.style.display = "block";
+//     modalContent[1].style.display = "flex";
+// }
 
-close[0].onclick = function () {
-    modalWrapper.style.display = "none";
-}
+// close[0].onclick = function () {
+//     modalWrapper.style.display = "none";
+// }
 
-close[1].onclick = function () {
-    modalWrapper.style.display = "none";
-}
+// close[1].onclick = function () {
+//     modalWrapper.style.display = "none";
+// }
 
-window.onclick = function (event) {
-    if (event.target == modalWrapper) {
-        modalWrapper.style.display = "none";
-    }
-}
+// window.onclick = function (event) {
+//     if (event.target == modalWrapper) {
+//         modalWrapper.style.display = "none";
+//     }
+// }
